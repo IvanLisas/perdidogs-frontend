94832de4c5598736a4fae7b55a18e5dbb98436f5
IvanLisas <ivanelisas@gmail.com>
2021-04-08 00:26:47 -0300

    first commit

---
+++ components/Test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Text, TextProps } from './Themed';
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <Text>Open Menu</Text>
+      </Button>
+    </div>
+  );
+}
