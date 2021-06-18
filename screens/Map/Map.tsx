f964b62f9681a8ae0e7b069f3ca957573550b396
IvanLisas <ivanelisas@gmail.com>
2021-06-17 23:27:34 -0300

    Fixes

--- screens/Map/Map.tsx
+++ screens/Map/Map.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Keyboard, StyleSheet, TouchableWithoutFeedback, View, Text, TouchableOpacity } from 'react-native'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { Keyboard, StyleSheet, TouchableWithoutFeedback, View, Text, TouchableOpacity, Button } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
@@ -16,6 +16,8 @@ import { ScrollView, TextInput } from 'react-native'
 import { GoogleAutoComplete } from 'react-native-google-autocomplete'
 import useTheme from '../../hooks/useTheme'
 import { Post } from '../../types/models/Post'
+import BottomSheetModal from './BottomSheetModal'
+import { BottomSheetModal as DefaultBottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 
 export default function Map() {
   const colorMode = Appearance.getColorScheme()
@@ -34,27 +36,37 @@ export default function Map() {
 
   const { mapRef, selectedMarker, handleNavigateToPoint, handelResetInitialPosition } = useMap()
 
+  const ref = useRef<DefaultBottomSheetModal>(null)
+
   const [marker, setMarker] = useState()
 
-  const post1 = { Id: 1, description: 'Post numero 1', location: { Id: 1, x: -34.54289695652187, y: -58.54144144943036 } } as Post
-  const post2 = { Id: 2, description: 'Post numero 2', location: { Id: 1, x: -34.54309695652187, y: -58.54244144943036 } } as Post
-  const post3 = { Id: 3, description: 'Post numero 3', location: { Id: 1, x: -34.54309695652187, y: -58.53244144943036 } } as Post
-  const post4 = { Id: 4, description: 'Post numero 4', location: { Id: 1, x: -34.54309695652187, y: -58.52244144943036 } } as Post
+  const post1 = { Id: 13, description: 'Post numero 1', location: { Id: 1, x: -34.54289695652187, y: -58.54144144943036 } } as Post
+  const post2 = { Id: 24, description: 'Post numero 2', location: { Id: 1, x: -34.54309695652187, y: -58.54244144943036 } } as Post
+  const post3 = { Id: 35, description: 'Post numero 3', location: { Id: 1, x: -34.54309695652187, y: -58.53244144943036 } } as Post
+  const post4 = { Id: 46, description: 'Post numero 4', location: { Id: 1, x: -34.54309695652187, y: -58.52244144943036 } } as Post
   const posts: Post[] = [post1, post2, post3, post4]
+
+  const snapPoints = useMemo(() => ['30%', '30%'], [])
 
   const findCurrentLocationAsync = async () => {
     await askPermissions()
-
+    handlePresentModalPress()
     /*   if (status !== 'granted') {
       this.setState({
         errorMessage: 'Permission to access location was denied'
       })
     } */
-
+    ref.current?.present()
     let location = await Location.getCurrentPositionAsync({})
     handleNavigateToPoint(1, location.coords.latitude, location.coords.longitude)
     setLocation(location)
   }
+
+  const handlePresentModalPress = useCallback(() => {
+    console.log('hola')
+    ref.current?.present()
+    /*     bottomSheetModalRef.current?.close() */
+  }, [])
 
   /*   useEffect(() => {
     findCoordinates()
@@ -62,101 +74,125 @@ export default function Map() {
  */
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={StyleSheet.absoluteFillObject}>
-        {(console.disableYellowBox = true)}
-        <MapView
-          ref={mapRef}
-          showsMyLocationButton={false}
-          /*   mapPadding={{ top: 516, bottom: 16, left: 16, right: 16 }} */
-          showsUserLocation={true}
-          /* showsCompass={false} */
-          /*    showsPointsOfInterest={false}
+      <BottomSheetModalProvider>
+        <View style={StyleSheet.absoluteFillObject}>
+          {(console.disableYellowBox = true)}
+          <MapView
+            ref={mapRef}
+            showsMyLocationButton={false}
+            /*   mapPadding={{ top: 516, bottom: 16, left: 16, right: 16 }} */
+            showsUserLocation={true}
+            /* showsCompass={false} */
+            /*    showsPointsOfInterest={false}
           showsBuildings={false}
           showsScale={false} */
-          showsPointsOfInterest={false}
-          customMapStyle={colorMode === 'dark' ? mapStyleNight : mapStyleDay}
-          provider={PROVIDER_GOOGLE}
-          /*    loadingEnabled={true} */
+            showsPointsOfInterest={false}
+            customMapStyle={colorMode === 'dark' ? mapStyleNight : mapStyleDay}
+            provider={PROVIDER_GOOGLE}
+            /*    loadingEnabled={true} */
 
-          style={StyleSheet.absoluteFillObject}
-          initialRegion={{
-            latitude: -34.535532,
-            longitude: -58.541518,
-            latitudeDelta: 0.003,
-            longitudeDelta: 0.003
-          }}
-          /*   mapType="standard" */
-          onPress={() => Keyboard.dismiss()}
-        >
-          {posts.map((post, index) => (
-            <Marker key={post.Id} coordinate={{ latitude: post.location.x, longitude: post.location.y }} title={post.description} />
-          ))}
-        </MapView>
+            style={StyleSheet.absoluteFillObject}
+            initialRegion={{
+              latitude: -34.535532,
+              longitude: -58.541518,
+              latitudeDelta: 0.003,
+              longitudeDelta: 0.003
+            }}
+            /*   mapType="standard" */
+            onPress={() => {
+              Keyboard.dismiss()
+              ref.current?.close()
+            }}
+          >
+            {posts.map((post, index) => (
+              <Marker
+                onPress={() => {
+                  handleNavigateToPoint(index, post.location.x, post.location.y)
+                  ref.current?.present()
+                }}
+                key={post.Id}
+                coordinate={{ latitude: post.location.x, longitude: post.location.y }}
+                title={post.description}
+              />
+            ))}
+          </MapView>
 
-        {/*   <View style={{ position: 'absolute', top: 100 }} /> */}
-        <TouchableOpacity style={{ position: 'absolute', top: 200 }} onPress={findCurrentLocationAsync}>
-          <Text> Mi Ubicacion </Text>
-        </TouchableOpacity>
-        <View style={{ position: 'absolute', padding: 16, top: 20, width: '100%' }}>
-          <GooglePlacesAutocomplete
-            placeholder="Buscar"
-            /*  currentLocation={true} */
-            textInputProps={{ placeholderTextColor: colors.text }}
-            fetchDetails={true}
-            suppressDefaultStyles={true}
-            /* onNotFound={() => <Text>No se encontaron resultados</Text>} */
-            /*   currentLocation={true} */
-            styles={{
-              textInputContainer: {
-                backgroundColor: colors.background,
-                borderRadius: 16
-              },
-              textInput: {
-                padding: 12,
-                placeholderTextColor: 'black',
-                fontFamily: 'LoveMeLikeASister',
-                paddingLeft: 18,
-                color: colors.text
-              },
-              predefinedPlacesDescription: {
-                color: '#1faadb'
-              },
-              listView: {
-                borderRadius: 16,
-                marginTop: 8,
-                backgroundColor: colors.background,
-                /*   padding: 16, */
+          {/*   <View style={{ position: 'absolute', top: 100 }} /> */}
+          <TouchableOpacity style={{ position: 'absolute', top: 200 }} onPress={findCurrentLocationAsync}>
+            <Text> Mi Ubicacion </Text>
+          </TouchableOpacity>
+          <View style={{ position: 'absolute', padding: 16, top: 20, width: '100%' }}>
+            <GooglePlacesAutocomplete
+              placeholder="Buscar"
+              /*  currentLocation={true} */
+              textInputProps={{ placeholderTextColor: colors.text }}
+              fetchDetails={true}
+              suppressDefaultStyles={true}
+              renderRightButton={cargas el boton}
+              /* onNotFound={() => <Text>No se encontaron resultados</Text>} */
+              /*   currentLocation={true} */
+              styles={{
+                textInputContainer: {
+                  backgroundColor: colors.background,
+                  borderRadius: 16
+                },
+                textInput: {
+                  padding: 12,
+                  placeholderTextColor: 'black',
+                  fontFamily: 'LoveMeLikeASister',
+                  paddingLeft: 18,
+                  color: colors.text
+                },
+                predefinedPlacesDescription: {
+                  color: '#1faadb'
+                },
+                listView: {
+                  borderRadius: 16,
+                  marginTop: 8,
+                  backgroundColor: colors.background,
+                  /*   padding: 16, */
 
-                overflow: 'scroll'
-              },
-              description: {
-                fontSize: 12,
-                padding: 16,
-                color: colors.text
-              },
-              separator: {
-                height: 0.5,
-                backgroundColor: '#c8c7cc'
-              },
-              poweredContainer: {
-                alignItems: 'flex-end',
-                padding: 16,
-                width: '100%'
-              }
-            }}
-            /*   styles={{ position: 'absolute', top: 300 }} */
-            onPress={(data, details = null) => {
-              console.log(data, details)
-              handleNavigateToPoint(1, details?.geometry.location.lat, details?.geometry.location.lng)
-            }}
-            query={{
-              key: 'AIzaSyCahzx0wpr4G7jiI_LfsAUf0JWJ3-FZVDs',
-              language: 'es',
-              components: 'country:arg'
-            }}
-          />
+                  overflow: 'scroll'
+                },
+                description: {
+                  fontSize: 12,
+                  padding: 16,
+                  color: colors.text
+                },
+                separator: {
+                  height: 0.5,
+                  backgroundColor: '#c8c7cc'
+                },
+                poweredContainer: {
+                  alignItems: 'flex-end',
+                  padding: 16,
+                  width: '100%'
+                }
+              }}
+              /*   styles={{ position: 'absolute', top: 300 }} */
+              onPress={(data, details, index = null) => {
+                console.log(data, details)
+                handleNavigateToPoint('g' + index, details?.geometry.location.lat, details?.geometry.location.lng)
+              }}
+              query={{
+                key: 'AIzaSyCahzx0wpr4G7jiI_LfsAUf0JWJ3-FZVDs',
+                language: 'es',
+                components: 'country:arg'
+              }}
+            />
+          </View>
+          <View style={{ position: 'absolute', padding: 16, bottom: 0, width: '100%' }}>
+            <View style={styles.container}>
+              {/*    <Button onPress={handlePresentModalPress} title="Present Modal" color="black" /> */}
+              <DefaultBottomSheetModal ref={ref} index={1} snapPoints={snapPoints}>
+                <View style={styles.contentContainer}>
+                  <Text>hola</Text>
+                </View>
+              </DefaultBottomSheetModal>
+            </View>
+          </View>
         </View>
-      </View>
+      </BottomSheetModalProvider>
     </TouchableWithoutFeedback>
   )
 }
@@ -188,5 +224,15 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: 'white'
     /*   alignSelf: 'center', */
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center'
+    /*   backgroundColor: 'grey' */
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center'
   }
 })
