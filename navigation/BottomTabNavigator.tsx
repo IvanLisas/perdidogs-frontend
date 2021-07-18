5563468fdb9089f39495a869cee5fdf06b82d3cd
Ivan <ivanelisas@gmail.com>
2021-07-18 04:22:12 -0300

    pan

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import BottomTabParamList from '../types/StackParamLists/BottomTabParamList'
 import useTheme from '../hooks/useTheme'
@@ -13,42 +13,44 @@ const Tab = createBottomTabNavigator<BottomTabParamList>()
 export default function BottomTabNavigator() {
   const theme = useTheme()
   return (
-    <Tab.Navigator
-      tabBarOptions={{
-        inactiveTintColor: 'grey',
-        /*  keyboardHidesTabBar: true, */
-        activeTintColor: theme.primary,
-        showLabel: false,
-        tabStyle: { backgroundColor: theme.navigation, borderTopWidth: 0.6 },
-        style: {
-          backgroundColor: theme.navigation,
-          borderRadius: 16
-        }
-      }}
-      initialRouteName="Mapa"
-    >
-      <Tab.Screen
-        name="Chats"
-        component={ChatsStackScreen}
-        options={{
-          tabBarIcon: ({ color }) => <Icon style={[{ color: color }, styles.icon]} name="chat-bubbles-couple-hand-drawn-outlines" />
+    <KeyboardAvoidingView style={{ flex: 1 }} enabled={true}>
+      <Tab.Navigator
+        tabBarOptions={{
+          inactiveTintColor: 'grey',
+          /*  keyboardHidesTabBar: true, */
+          activeTintColor: theme.primary,
+          showLabel: false,
+          tabStyle: { backgroundColor: theme.navigation, borderTopWidth: 0.6 },
+          style: {
+            backgroundColor: theme.navigation,
+            borderRadius: 16
+          }
         }}
-      />
-      <Tab.Screen
-        name="Mapa"
-        component={MapStackScreen}
-        options={{
-          tabBarIcon: ({ color }) => <Icon style={[{ color: color }, styles.icon]} name="compass-hand-drawn-circular-tool-outline" />
-        }}
-      />
-      <Tab.Screen
-        name="Perfil"
-        component={ProfileStackScreen}
-        options={{
-          tabBarIcon: ({ color }) => <Icon style={[{ color: color }, styles.icon]} name="user-list-hand-drawn-interface-symbol-outline" />
-        }}
-      />
-    </Tab.Navigator>
+        initialRouteName="Mapa"
+      >
+        <Tab.Screen
+          name="Chats"
+          component={ChatsStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => <Icon style={[{ color: color }, styles.icon]} name="chat-bubbles-couple-hand-drawn-outlines" />
+          }}
+        />
+        <Tab.Screen
+          name="Mapa"
+          component={MapStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => <Icon style={[{ color: color }, styles.icon]} name="compass-hand-drawn-circular-tool-outline" />
+          }}
+        />
+        <Tab.Screen
+          name="Perfil"
+          component={ProfileStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => <Icon style={[{ color: color }, styles.icon]} name="user-list-hand-drawn-interface-symbol-outline" />
+          }}
+        />
+      </Tab.Navigator>
+    </KeyboardAvoidingView>
   )
 }
 
--- screens/ChatConversation.tsx
+++ screens/ChatConversation.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet, View } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native'
 import UserContext from '../contexts/UserContext'
 import { Avatar } from 'react-native-elements'
 import chatService from '../services/ChatService'
@@ -12,12 +12,14 @@ import ChatContext from '../contexts/ChatsContext'
 import { Chat } from '../types/models/Chat'
 import { User } from '../types/models/User'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { ScrollView } from 'react-native-gesture-handler'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
+import { flexWrap } from 'styled-system'
 
 const ChatConversation: React.FC = (props) => {
   const { user } = useContext(UserContext)
   const { chats, findChatId } = useContext(ChatContext)
   const route = useRoute()
+  const [scrollPosition, setScrollPosition] = useState(0)
 
   const addressee = (route.params as any).addressee as User
   const chatId = findChatId(addressee.Id)
@@ -34,65 +36,79 @@ const ChatConversation: React.FC = (props) => {
   const theme = useTheme()
 
   if (!user) return null
-  const scrollViewRef = useRef<any>()
+  const scrollViewRef = useRef<ScrollView>(null)
   return (
-    <View style={styles.root}>
-      <ScrollView ref={scrollViewRef} onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}>
-        {chatId && (
-          <View style={{ padding: 16 }}>
-            {chat.messageList.map((message: any, index: number) =>
-              message.sender.Id == user.Id ? (
-                <View key={index + 'avatar2'} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 24, alignSelf: 'flex-end' }}>
-                  <MyText
-                    key={index + 'avatar3'}
-                    style={{
-                      color: 'white',
-                      marginRight: 8,
-                      maxWidth: 300,
-                      padding: 16,
-                      borderRadius: 24,
-                      borderTopRightRadius: 0,
-                      backgroundColor: '#1868FB'
-                    }}
-                  >
-                    {message.body}
-                  </MyText>
-                  <Avatar
-                    key={index + 'avatar'}
-                    size="medium"
-                    titleStyle={{ color: 'white' }}
-                    source={{ uri: message.sender.avatar }}
-                    overlayContainerStyle={{ backgroundColor: 'grey' }}
-                    rounded
-                    title={user.firstName[0] + user.lastName[0]}
-                  />
-                </View>
-              ) : (
-                <View key={index + 'avatar4'} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 24, alignSelf: 'flex-start' }}>
-                  <Avatar
-                    key={index + 'avatar6'}
-                    size="medium"
-                    titleStyle={{ color: 'white' }}
-                    source={{ uri: message.sender.avatar }}
-                    overlayContainerStyle={{ backgroundColor: 'grey' }}
-                    rounded
-                    containerStyle={{ marginRight: 8 }}
-                    title={user.firstName[0] + user.lastName[0]}
-                  />
-                  <MyText
-                    style={{ color: 'white', maxWidth: 300, padding: 16, borderRadius: 24, borderTopLeftRadius: 0, backgroundColor: '#00BF9D' }}
-                  >
-                    {message.body}
-                  </MyText>
-                </View>
-              )
-            )}
-          </View>
-        )}
-      </ScrollView>
+    <KeyboardAvoidingView style={styles.root} behavior={Platform.OS == 'ios' ? 'padding' : 'height'} enabled={false}>
+      <View style={styles.root}>
+        <KeyboardAwareScrollView>
+          {chatId && (
+            <View style={{ padding: 16 }}>
+              {chat.messageList.map((message: any, index: number) =>
+                message.sender.Id == user.Id ? (
+                  <View key={index + 'avatar2'} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 24, alignSelf: 'flex-end' }}>
+                    <MyText
+                      key={index + 'avatar3'}
+                      style={{
+                        color: 'white',
+                        marginRight: 8,
+                        maxWidth: 300,
+                        padding: 16,
+                        borderRadius: 24,
+                        borderTopRightRadius: 0,
+                        backgroundColor: '#1868FB'
+                      }}
+                    >
+                      {message.body}
+                    </MyText>
+                    <Avatar
+                      key={index + 'avatar'}
+                      size="medium"
+                      titleStyle={{ color: 'white' }}
+                      source={{ uri: message.sender.avatar }}
+                      overlayContainerStyle={{ backgroundColor: 'grey' }}
+                      rounded
+                      title={user.firstName[0] + user.lastName[0]}
+                    />
+                  </View>
+                ) : (
+                  <View key={index + 'avatar4'} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 24, alignSelf: 'flex-start' }}>
+                    <Avatar
+                      key={index + 'avatar6'}
+                      size="medium"
+                      titleStyle={{ color: 'white' }}
+                      source={{ uri: message.sender.avatar }}
+                      overlayContainerStyle={{ backgroundColor: 'grey' }}
+                      rounded
+                      containerStyle={{ marginRight: 8 }}
+                      title={user.firstName[0] + user.lastName[0]}
+                    />
+                    <MyText
+                      style={{ color: 'white', maxWidth: 300, padding: 16, borderRadius: 24, borderTopLeftRadius: 0, backgroundColor: '#00BF9D' }}
+                    >
+                      {message.body}
+                    </MyText>
+                  </View>
+                )
+              )}
+            </View>
+          )}
+        </KeyboardAwareScrollView>
 
-      <SendAMessageBar onPress={sendMessage} setText={setText} text={text}></SendAMessageBar>
-    </View>
+        <View
+          style={{
+            flexWrap: 'wrap',
+            alignItems: 'flex-start',
+            flexDirection: 'row',
+            position: 'absolute',
+            bottom: 0
+          }}
+        >
+          {/*   <TextInput /> */}
+          <SendAMessageBar onPress={sendMessage} setText={setText} text={text}></SendAMessageBar>
+        </View>
+        {/* <SendAMessageBar onPress={sendMessage} setText={setText} text={text}></SendAMessageBar> */}
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -100,7 +116,9 @@ export default ChatConversation
 
 const styles = StyleSheet.create({
   root: {
+    /*  flexDirection: 'column' */
     flex: 1
+
     /*  padding: 16 */
     /* 
     alignItems: 'center',
